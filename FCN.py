6f540b353346b8768b0686c283981f84f977b3b3
chico <franchuterivera@gmail.com>
2018-11-18 20:29:29 +0100

    merge ident issue

--- FCN.py
+++ FCN.py
@@ -7,7 +7,7 @@ import numpy as np
 import tensorflow as tf
 from loss import *
 from metrics import *
-from nets_definition import * 
+from nets_definition import *
 
 class FCN_SS(object):
 	def __init__(self):
@@ -17,15 +17,15 @@ class FCN_SS(object):
 		opt=self.opt
 
 		print("ALL Ok GIRL")
-		print(opt.dataset_dir) 
-		
+		print(opt.dataset_dir)
+
 		#class weights Sitting
 		class_weight = [ 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0]
 		class_weight = tf.reshape(tf.convert_to_tensor(class_weight, dtype=tf.float32), [12])
 
-		self.train_image_batch = tf.placeholder(tf.float32, [None, opt.img_height, opt.img_width, 3])	
+		self.train_image_batch = tf.placeholder(tf.float32, [None, opt.img_height, opt.img_width, 3])
 		self.train_label_batch = tf.placeholder(tf.float32, [None, opt.img_height*opt.img_width, opt.numberClasses])
-		
+
 		self.tgt_image = self.train_image_batch
 		self.tgt_label = self.train_label_batch
 		self.tgt_label = self.train_label_batch
@@ -48,7 +48,7 @@ class FCN_SS(object):
 				segMap = FCN_Seg(self, is_training=self.Training)
 			print("Output FCN_Seg")
 			print(segMap)
-			
+
 		with tf.name_scope("Output_Metrics"):
 			segmentationMask = tf.argmax(segMap,axis=1)
 			# 	print("Segmentation mask")
@@ -62,88 +62,88 @@ class FCN_SS(object):
 			print(segmentationLabel)
 			equality = tf.equal(Smask, segmentationLabel)
 			accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
-			
-			
+
+
 		with tf.name_scope("compute_loss"):
 			#Compute Softmax loss of D-Net
 			Reshaped_labels = tf.reshape(self.train_label_batch, (-1, self.N_classes))
-						
+
 			if opt.version_net =='FCN_Seg':
 				TotalLoss = loss(segMap, Reshaped_labels, self.N_classes,class_weight)
-				
+
 			print(TotalLoss)
 
 		with tf.name_scope("Training"):
 			print("==================== Training =================================")
-			train_vars = [var for var in tf.trainable_variables()] 
-			
+			train_vars = [var for var in tf.trainable_variables()]
+
 			global_step = tf.train.get_or_create_global_step()
 			self.global_step = global_step
 			self.incr_global_step = tf.assign(self.global_step, self.global_step+1)
-			
+
 			############# DECAY ############################
 			print("Exponential Decay .......")
 			Decay=0.90
 			# Decay the learning rate exponentially based on the number of steps.
 			lr = tf.train.exponential_decay(opt.learning_rate, global_step,	(self.total_steps/2), Decay, staircase=True)
-			
-			############ OPTIMIZER #########################		
-			#select Adam as optimizer 
+
+			############ OPTIMIZER #########################
+			#select Adam as optimizer
 			optimizer = tf.train.AdamOptimizer(lr, opt.beta1)
-			
+
 			self.grads_and_vars = optimizer.compute_gradients(TotalLoss, var_list=train_vars)
 			self.Training = optimizer.apply_gradients(self.grads_and_vars)
 			self.learning_rate = lr
-			
+
 
 		#Collect tensors that are useful later (tf summary)
 		self.predMask = segMap
 		self.total_loss = TotalLoss
 		self.steps_per_epoch = opt.steps_per_epoch
 		self.accuracy = accuracy
-		
+
 		self.version_net = opt.version_net
-		
-		# merge all summaries into a single "operation" which we can execute in a session 
+
+		# merge all summaries into a single "operation" which we can execute in a session
 		#merged = tf.summary.merge_all()
 		self.logs_path = opt.logs_path
-		
+
 		print("DONE BUILDING GRAPH!")
 
 	def collect_summaries(self):
 		opt = self.opt
-		
+
 		#losses
 		tf.summary.scalar("total_loss", self.total_loss)
 		tf.summary.scalar("accuracy", self.accuracy)
-		
+
 	def Load_TrainDataset(self):
 			print("Loading training Set")
 			opt=self.opt
-			#build train and val sets 
+			#build train and val sets
 			#Train SET
 			self.img_width=opt.img_width
 			self.img_height=opt.img_height
 			train_path = opt.dataset_dir + 'Train_data_' + opt.dataset + '.npy'
 			#print(train_path)
 			train_data = np.load(train_path)
-			train_data = train_data.reshape((train_data.shape[0], opt.img_height, opt.img_width, 3))	
+			train_data = train_data.reshape((train_data.shape[0], opt.img_height, opt.img_width, 3))
 			print(train_data.shape)
-			
-			train_label_path = opt.dataset_dir + 'Train_label_' + opt.dataset + '.npy' 
+
+			train_label_path = opt.dataset_dir + 'Train_label_' + opt.dataset + '.npy'
 			train_label =  np.load(train_label_path)
 			train_label = train_label.reshape((train_data.shape[0], opt.img_height*opt.img_width, 1))
 			# shape is (376, 50176, 12)
 			print(train_label.shape)
 
 			return train_data, train_label
-	
+
 	def Create_batches(self, train_data, train_label):
-		
+
 		train_image_batch=np.zeros((self.batch_size, self.img_height, self.img_width, 3), dtype=np.float32)
 		#train_label_batch=np.zeros((self.batch_size, self.img_height*self.img_width, self.N_classes), dtype=np.float32)
 		train_label_batch=np.zeros((self.batch_size, self.img_height*self.img_width, self.N_classes), dtype=np.float32)
-		
+
 		self.random=True
 
 
@@ -157,22 +157,22 @@ class FCN_SS(object):
 		#print(train_label_batch.shape)
 		for i in range(self.batch_size):
 			#train sample
-			if (self.random==False): 
+			if (self.random==False):
 				train_image_batch[i,:,:,:] = train_data[self.counter+i, :, :, :]
-				#Test Sample 
+				#Test Sample
 				train_label_batch[i,:,:] = self.unfould(train_label[self.counter+i, :, :])
 			else:
 				#print("RANDOM TRAINING :)")
 				index = random.randint(0, train_data.shape[0] -1)
 				train_image_batch[i,:,:,:] = train_data[index, :, :, :]
-				#Test Sample 
+				#Test Sample
 				train_label_batch[i,:,:] = self.unfould(train_label[index, :, :])
 				#train_label_batch[i,:,:] = (train_label[index, :, :])
 
 
 		#train_label_batch = tf.reshape(train_label_batch, (-1, self.batch_size*self.img_height*self.img_width))
 		return train_image_batch, train_label_batch
-	
+
 	def unfould(self, image):
 		#print(image.shape)
 		image = np.reshape(image, (1, self.height, self.width, 1))
@@ -186,7 +186,7 @@ class FCN_SS(object):
 				# else:
 				# 	label_training[0,row,column,int(image[0,row,column,0])] = 1
 		label_training = np.reshape(label_training, (1, self.height*self.width, self.N_classes))
-		#print(label_training.shape)			
+		#print(label_training.shape)
 		return label_training
 
 	def train(self, opt):
@@ -199,10 +199,10 @@ class FCN_SS(object):
 		self.collect_summaries()
 
 		# Merge all summary inforation.
-  		summary = tf.summary.merge_all()
+		summary = tf.summary.merge_all()
 
 		print("=================Collection Variables ok!!!!==========================")
-		
+
 
 		#self.setup_inference(opt.version_net, opt.img_height, opt.img_width, opt.batch_size_val)
 		#print("Building Validation Graph OK!!!")
@@ -212,16 +212,16 @@ class FCN_SS(object):
 		print("Validation Set DONE!")
 		print(imgs.shape)
 		print(label.shape)
-		
+
 		Accuracy_validation = 0.0
 		Iou_validation = 0.0
-		
+
 		with tf.name_scope("parameter_count"):
 			parameter_count = tf.reduce_sum([tf.reduce_prod(tf.shape(v)) for v in tf.trainable_variables()])
-		
+
 		self.saver = tf.train.Saver([var for var in tf.trainable_variables()], max_to_keep=100)
 		self.saver2 = tf.train.Saver([var for var in tf.trainable_variables()] + [self.global_step], max_to_keep=100)
-		
+
 
 		sv = tf.train.Supervisor(logdir=opt.logs_path, save_summaries_secs=0, saver=None)
 		print("passed")
@@ -236,7 +236,7 @@ class FCN_SS(object):
 			for var in tf.trainable_variables():
 				print(var.name)
 			print("parameter_count =", sess.run(parameter_count))
-			#restore the latest model - 
+			#restore the latest model -
 			if opt.continue_train:
 				print("Resuming training ")
 				checkpoint = tf.train.latest_checkpoint(opt.checkpoint_dir)
@@ -253,29 +253,29 @@ class FCN_SS(object):
 			##Load Training dataset
 			train_data, train_label = self.Load_TrainDataset()
 
-			
+
 			print("=================== Starting Iterations =========================")
 			global_start_time = time.time()
 			for step in range(1, opt.max_steps):
 				start_time = time.time()
 				fetches = {"Training": self.Training, "global_step": self.global_step, "incr_global_step": self.incr_global_step}
-				
+
 				#train_image_batch, train_label_batch = self.Create_batches(train_data, train_label)
 				train_image_batch, train_label_batch = self.Create_batches(train_data, train_label)
 
-				
+
 				#train_image_batch_ = np.reshape(train_image_batch, (opt.batch_size, opt.img_height*opt.img_width*3))
 				#train_label_batch_ = np.reshape(train_label_batch, (opt.batch_size, opt.img_height*opt.img_width*12))
 
 				if step % opt.summary_freq == 0:
 					fetches["loss"] = self.total_loss
-					
+
 				fetches["learning_rate"] = self.learning_rate
 				fetches["loss_iter"] = self.total_loss
 				#fetches["merged"] = self.merged
 				fetches["summary"] = sv.summary_op
-				
-				
+
+
 				#self.Training=True
 				#self.Set_batch_size(sess, opt.batch_size)
 				#print("Input values")
@@ -286,9 +286,9 @@ class FCN_SS(object):
 				LR = results["learning_rate"]
 				#summ = results["merged"]
 
-				#print(results["loss"]) 
-				TotalLOSS = TotalLOSS + results["loss_iter"] 
-				
+				#print(results["loss"])
+				TotalLOSS = TotalLOSS + results["loss_iter"]
+
 				# write log
 				#writer.add_summary(summary, step)
 				#train_writer.add_summary(summ, step)
@@ -308,8 +308,8 @@ class FCN_SS(object):
 				U_tot = 0.0
 
 				NumberIterations_train_to_test=((opt.steps_per_epoch))
-				
-				#Saving model 
+
+				#Saving model
 				if ((step % (NumberIterations_train_to_test*2)) == 0): # and (Accuracy_validation < Global_IoU):
 					Accuracy_validation = Global_IoU
 					#Save all Variables
@@ -328,7 +328,7 @@ class FCN_SS(object):
 			#save all Variables
 			self.saver2.save(sess, os.path.join(checkpoint_dir, model_name), global_step=step)
 
-				
+
 	def setup_inference(self, version_net, img_height, img_width, batch_size, Test, N_classes, dataset, configuration):
 		self.img_height = img_height
 		self.img_width = img_width
@@ -340,7 +340,7 @@ class FCN_SS(object):
 		self.configuration = configuration
 		#Call build test_graph
 		self.build_test_graph()
-		
+
 
 
 	def build_test_graph(self):
@@ -352,40 +352,40 @@ class FCN_SS(object):
 		self.width = self.img_width
 		self.height = self.img_height
 		self.class_balance = True
-		
+
 
 		with tf.name_scope("segmentation_prediction"):
 			if self.version_net == 'FCN_Seg':
-				segMap = FCN_Seg(self, is_training=True)	
+				segMap = FCN_Seg(self, is_training=True)
 			print("Output FCN_Seg")
 			print(segMap)
 
 		self.inputs = input_img
 		self.predMask = segMap
-		
 
-	
+
+
 	def inference(self, inputs, sess):
 		fetches = {}
-		inputs = inputs.reshape((1, self.height, self.width, 3))	
+		inputs = inputs.reshape((1, self.height, self.width, 3))
 		fetches['Mask'] = self.predMask
 		results = sess.run(fetches, feed_dict={self.inputs:inputs})
 		return results
 
-		
+
 
 	def loadTest_set(self, opt):
-		# #Test SET 
+		# #Test SET
 		test_path = opt.dataset_dir + 'Test_data_' + opt.dataset + '.npy'
 		#test_path = opt.dataset_dir + 'RealTest_data_Kitti.npy'
-		
+
 		print(test_path)
 		test_data = np.load(test_path)
-		test_data = test_data.reshape((test_data.shape[0], self.height, self.width, 3))	
+		test_data = test_data.reshape((test_data.shape[0], self.height, self.width, 3))
 		print(test_data.shape)
-		
+
 		test_label_path = opt.dataset_dir + 'Test_label_' + opt.dataset + '.npy'
-		print(test_label_path) 
+		print(test_label_path)
 		test_label =  np.load(test_label_path)
 		test_label = test_label.reshape((test_label.shape[0], self.height*self.width, 1))
 
@@ -401,25 +401,25 @@ class FCN_SS(object):
 		return test_data, test_label_batch
 
 	def loadValidation_set(self, opt):
-		#Test SET 
+		#Test SET
 		#test_path = opt.dataset_dir + 'Test_data_' + 'Camvid2' + '.npy'
 		test_path = opt.dataset_dir + 'Test_data_' + opt.dataset + '.npy'
-		
+
 		print(test_path)
 		test_data = np.load(test_path)
-		test_data = test_data.reshape((test_data.shape[0], self.height, self.width, 3))	
+		test_data = test_data.reshape((test_data.shape[0], self.height, self.width, 3))
 		print(test_data.shape)
-		
+
 		#test_label_path = opt.dataset_dir + 'Test_label_' + 'Camvid2' + '.npy'
 		test_label_path = opt.dataset_dir + 'Test_label_' + opt.dataset + '.npy'
-		
-		print(test_label_path) 
+
+		print(test_label_path)
 		test_label =  np.load(test_label_path)
 		test_label = test_label.reshape((test_label.shape[0], self.height*self.width, 1))
 
 		print(test_label.shape)
 		## Not performing one-hot-encoding here anymore
-		
-		# return test_data, test_label_batch	
+
+		# return test_data, test_label_batch
 		return test_data, test_label
-	   
+
